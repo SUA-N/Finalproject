1d4afe3513d3e51edc2351289c6eba08b9bab8f7
SUA-N <vangarasukanya332@gmail.com>
2024-11-11 16:49:46 +0530

    c

---
+++ src/main/java/Basepackage/BaseClass.java
@@ -0,0 +1,30 @@
+package Basepackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	 public static WebDriver Driver;
+	 
+	 
+		public static void application_launch()
+		{	
+			//WebDriverManager.chromedriver().setup();	
+			System.setProperty("webdriver.chrome.driver","C://chromedriver.exe");
+
+			Driver= new ChromeDriver();
+			Driver.get("https://www.fb.com");	
+		}
+		
+		public static void APplication_close()
+		{
+			
+			Driver.close();
+		}
+		
+	}
